2e667542bf2b7854a6a6efbb0c76fb912c299b87
Jihye Park <deviloper.park@gmail.com>
2024-04-14 17:01:09 +0900

    Initial Commit

---
+++ timer-for-appschool/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  timer-for-appschool
+//
+//  Created by 박지혜 on 4/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ timer-for-appschool/timer_for_appschoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  timer_for_appschoolApp.swift
+//  timer-for-appschool
+//
+//  Created by 박지혜 on 4/14/24.
+//
+
+import SwiftUI
+
+@main
+struct timer_for_appschoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
